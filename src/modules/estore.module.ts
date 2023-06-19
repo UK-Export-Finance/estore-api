60da4a147549a06d1edcecdc2884d38e3b0d021c
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-06-19 12:05:14 +0100

    fix(APIM-450): fix module imports

--- src/modules/estore.module.ts
+++ src/modules/estore.module.ts
@@ -5,8 +5,9 @@ import { GraphModule } from './graph/graph.module';
 import { GraphClientModule } from './graph-client/graph-client.module';
 import { SiteModule } from './site/site.module';
 import { TermsModule } from './terms/terms.module';
+import { TemporaryFileCheckModule } from './temporary-file-check/temporary-file-check.module';
 
 @Module({
-  imports: [AuthModule, GraphClientModule, GraphModule, SiteModule, TermsModule],
+  imports: [AuthModule, GraphClientModule, GraphModule, SiteModule, TemporaryFileCheckModule, TermsModule],
 })
 export class EstoreModule {}
