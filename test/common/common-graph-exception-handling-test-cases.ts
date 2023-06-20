2f348aafa9df4fa3a816ce95846ea2febda22d70
Alex Bramhill <abramhill@ukexportfinance.gov.uk>
2023-06-20 11:16:18 +0100

    feat(APIM-134): fix api tests

--- test/common/common-graph-exception-handling-test-cases.ts
+++ test/common/common-graph-exception-handling-test-cases.ts
@@ -3,8 +3,28 @@ import { GraphInvalidRequestException } from '../../src/modules/graph/exception/
 import { GraphUnexpectedException } from '../../src/modules/graph/exception/graph-unexpected.exception';
 
 export const commonGraphExceptionTestCases = [
-  { graphErrorCode: 'CredentialUnavailableError', expectedError: GraphAuthenticationFailedException, expectedResponseCode: 500, expectedResponseMessage: 'Internal server error' },
-  { graphErrorCode: 'AuthenticationRequiredError', expectedError: GraphAuthenticationFailedException, expectedResponseCode: 500, expectedResponseMessage: 'Internal server error' },
-  { graphErrorCode: 'invalidRequest', expectedError: GraphInvalidRequestException, expectedResponseCode: 500, expectedResponseMessage: 'Internal server error' },
-  { graphErrorCode: 'UnexpectedErrorCode', expectedError: GraphUnexpectedException, expectedResponseCode: 500, expectedResponseMessage: 'Internal server error' },
+  {
+    graphErrorCode: 'CredentialUnavailableError',
+    expectedError: GraphAuthenticationFailedException,
+    expectedResponseCode: 500,
+    expectedResponseMessage: 'Internal server error',
+  },
+  {
+    graphErrorCode: 'AuthenticationRequiredError',
+    expectedError: GraphAuthenticationFailedException,
+    expectedResponseCode: 500,
+    expectedResponseMessage: 'Internal server error',
+  },
+  {
+    graphErrorCode: 'invalidRequest',
+    expectedError: GraphInvalidRequestException,
+    expectedResponseCode: 500,
+    expectedResponseMessage: 'Internal server error',
+  },
+  {
+    graphErrorCode: 'UnexpectedErrorCode',
+    expectedError: GraphUnexpectedException,
+    expectedResponseCode: 500,
+    expectedResponseMessage: 'Internal server error',
+  },
 ];
