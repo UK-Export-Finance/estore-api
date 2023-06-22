4680a71df3b33b61e838b61a68a3c586355f3e30
AlexBramhill <47955140+AlexBramhill@users.noreply.github.com>
2023-06-22 16:11:46 +0100

    Feat/apim 134/migrate get site exists endpoint (#59)
    Co-authored-by: Alex Bramhill <abramhill@ukexportfinance.gov.uk>

--- src/modules/site/dto/get-site-status-by-exporter-name-query.dto.ts
+++ src/modules/site/dto/get-site-status-by-exporter-name-query.dto.ts
@@ -1,6 +1,13 @@
+import { EXPORTER_NAME } from '@ukef/constants/exporter-name.constant';
 import { ValidatedStringApiProperty } from '@ukef/decorators/validated-string-api-property.decorator';
 
 export class GetSiteStatusByExporterNameQueryDto {
-  @ValidatedStringApiProperty({ description: 'The name of the exporter used in the deal.', example: 'Example Name Limited', minLength: 1 })
+  @ValidatedStringApiProperty({
+    description: 'The name of the exporter used in the deal.',
+    example: 'Example Name Limited',
+    minLength: 1,
+    maxLength: 250,
+    pattern: EXPORTER_NAME.REGEX,
+  })
   exporterName: string;
 }
