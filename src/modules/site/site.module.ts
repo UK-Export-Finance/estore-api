173efda8d8d3f2c3d1bcce595f08ca08bde83074
lmarrai-sw <44811960+lmarrai-sw@users.noreply.github.com>
2023-06-22 17:17:23 +0100

    feat(APIM-133): create a site id via the mdm /numbers endpoint (#57)
    Co-authored-by: Francesca Stocco <FStocco@ukexportfinance.gov.uk>

--- src/modules/site/site.module.ts
+++ src/modules/site/site.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { GraphModule } from '@ukef/modules/graph/graph.module';
+import { MdmModule } from '@ukef/modules/mdm/mdm.module';
 
 import { SiteController } from './site.controller';
 import { SiteService } from './site.service';
 import { SiteExceptionTransformInterceptor } from './site-exception-transform.interceptor';
 
 @Module({
-  imports: [GraphModule],
+  imports: [GraphModule, MdmModule],
   controllers: [SiteController],
   providers: [
     SiteService,
---
+++ src/modules/site/site.service.create-site-id.test.ts
@@ -0,0 +1,50 @@
+import { RandomValueGenerator } from '@ukef-test/support/generator/random-value-generator';
+import { when } from 'jest-when';
+
+import { GraphService } from '../graph/graph.service';
+import { MdmService } from '../mdm/mdm.service';
+import { SiteService } from './site.service';
+
+jest.mock('../graph/graph.service');
+
+describe('SiteService', () => {
+  const valueGenerator = new RandomValueGenerator();
+
+  const ukefSharepointName = valueGenerator.string();
+  const tfisSiteName = valueGenerator.string();
+  const tfisListId = valueGenerator.string();
+  const maskedId = valueGenerator.stringOfNumericCharacters();
+
+  let siteService: SiteService;
+  let graphServiceGetRequest: jest.Mock;
+  let mdmServiceCreateNumbers: jest.Mock;
+
+  beforeEach(() => {
+    graphServiceGetRequest = jest.fn();
+    const graphService = new GraphService(null);
+    graphService.get = graphServiceGetRequest;
+
+    mdmServiceCreateNumbers = jest.fn();
+    const mdmService = new MdmService(null);
+    mdmService.createNumbers = mdmServiceCreateNumbers;
+    siteService = new SiteService({ ukefSharepointName, tfisSiteName, tfisListId }, graphService, mdmService);
+  });
+
+  describe('createSiteId', () => {
+    it('returns the masked id from creating a number of type 6 in MDM', async () => {
+      when(mdmServiceCreateNumbers)
+        .calledWith([
+          {
+            numberTypeId: 6,
+            createdBy: 'Estore',
+            requestingSystem: 'Estore',
+          },
+        ])
+        .mockResolvedValueOnce([{ maskedId }]);
+
+      const createdSiteId = await siteService.createSiteId();
+
+      expect(createdSiteId).toEqual(maskedId);
+    });
+  });
+});
