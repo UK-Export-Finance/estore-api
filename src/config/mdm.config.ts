db8da4118810601e1afafb382f0d481f2fff92d0
Luke Marrai <Luke.Marrai@softwire.com>
2023-06-22 14:42:38 +0100

    refactor(APIM-133): remove TODO

--- src/config/mdm.config.ts
+++ src/config/mdm.config.ts
@@ -16,7 +16,6 @@ export default registerAs(
     baseUrl: process.env.MDM_BASE_URL,
     apiKeyHeaderName: process.env.MDM_API_KEY_HEADER_NAME,
     apiKeyHeaderValue: process.env.MDM_API_KEY_HEADER_VALUE,
-    // TODO APIM-133: what about 0?
     maxRedirects: parseInt(process.env.MDM_MAX_REDIRECTS) || 5,
     timeout: parseInt(process.env.MDM_TIMEOUT) || 30000,
   }),
