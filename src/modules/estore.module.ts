e0da5cd839cacfd9274c4aeb631a6c3cd0c2dc14
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-06-27 09:56:32 +0100

    fix(APIM-450): fix after rebase

--- src/modules/estore.module.ts
+++ src/modules/estore.module.ts
@@ -4,8 +4,8 @@ import { AuthModule } from '@ukef/modules/auth/auth.module';
 import { GraphModule } from './graph/graph.module';
 import { GraphClientModule } from './graph-client/graph-client.module';
 import { SiteModule } from './site/site.module';
-import { TermsModule } from './terms/terms.module';
 import { TemporaryFileCheckModule } from './temporary-file-check/temporary-file-check.module';
+import { TermsModule } from './terms/terms.module';
 
 @Module({
   imports: [AuthModule, GraphClientModule, GraphModule, SiteModule, TemporaryFileCheckModule, TermsModule],
