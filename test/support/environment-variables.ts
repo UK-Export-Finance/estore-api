2211016979155850373d40f0d7e76933d48538e0
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-06-28 12:03:44 +0100

    feat(iac/env): added missing env variables

--- test/support/environment-variables.ts
+++ test/support/environment-variables.ts
@@ -4,14 +4,15 @@ const valueGenerator = new RandomValueGenerator();
 
 export const ENVIRONMENT_VARIABLES = Object.freeze({
   NODE_ENV: 'test',
+  TZ: 'Europe/London',
   LOG_LEVEL: 'debug',
 
   SWAGGER_USER: valueGenerator.string(),
   SWAGGER_PASSWORD: valueGenerator.string(),
+
   API_KEY: valueGenerator.string(),
 
   GRAPH_BASE_URL: valueGenerator.httpsUrl(),
-
   GRAPH_AUTHENTICATION_TENANT_ID: valueGenerator.string(),
   GRAPH_AUTHENTICATION_CLIENT_ID: valueGenerator.string(),
   GRAPH_AUTHENTICATION_CLIENT_SECRET: valueGenerator.string(),
