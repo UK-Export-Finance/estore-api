a446121c2d976b1740cf76396a2bfa1762d11156
Luke Marrai <Luke.Marrai@softwire.com>
2023-07-13 16:15:07 +0100

    feat(APIM-528): add unit tests for DocumentTypeMapper

--- src/modules/deal-folder/document-type-mapper.ts
+++ src/modules/deal-folder/document-type-mapper.ts
@@ -13,48 +13,39 @@ export class DocumentTypeMapper {
   ) {}
 
   mapDocumentTypeToTitleAndTypeId(documentType: DocumentTypeEnum): { documentTitle: string; documentTypeId: string } {
-    let result: { documentTitle: string; documentTypeId: string };
     switch (documentType) {
       case ENUMS.DOCUMENT_TYPES.EXPORTER_QUESTIONNAIRE:
-        result = {
+        return {
           documentTitle: 'Supplementary Questionnaire',
           documentTypeId: this.config.estoreDocumentTypeIdApplication,
         };
-        break;
       case ENUMS.DOCUMENT_TYPES.AUDITED_FINANCIAL_STATEMENTS:
-        result = {
+        return {
           documentTitle: 'Annual Report',
           documentTypeId: this.config.estoreDocumentTypeIdFinancialStatement,
         };
-        break;
       case ENUMS.DOCUMENT_TYPES.YEAR_TO_DATE_MANAGEMENT:
-        result = {
+        return {
           documentTitle: 'Financial Statement',
           documentTypeId: this.config.estoreDocumentTypeIdFinancialStatement,
         };
-        break;
       case ENUMS.DOCUMENT_TYPES.FINANCIAL_FORECASTS:
-        result = {
+        return {
           documentTitle: 'Financial Forecast',
           documentTypeId: this.config.estoreDocumentTypeIdFinancialStatement,
         };
-        break;
       case ENUMS.DOCUMENT_TYPES.FINANCIAL_INFORMATION_COMMENTARY:
-        result = {
+        return {
           documentTitle: 'Financial Commentary',
           documentTypeId: this.config.estoreDocumentTypeIdFinancialStatement,
         };
-        break;
       case ENUMS.DOCUMENT_TYPES.CORPORATE_STRUCTURE:
-        result = {
+        return {
           documentTitle: 'Corporate Structure Diagram',
           documentTypeId: this.config.estoreDocumentTypeIdBusinessInformation,
         };
-        break;
       default:
-        break;
+        return undefined;
     }
-
-    return result;
   }
 }
