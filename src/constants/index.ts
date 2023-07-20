7d4063d9f5260ea2bdb69f83f98e4ba6bcf1440a
Francesca Stocco <FStocco@ukexportfinance.gov.uk>
2023-07-20 17:08:08 +0100

    fix(apim-559): add pattern for site identifier & test

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,14 +2,18 @@
  * eStore centralised constants, referred as `@ukef/constants`.
  * Following constants are served:
  *
- * 1. AUTH
- * 2. DTFS_MAX_FILE_SIZE_BYTES
- * 2. ENUMS
- * 3. EXAMPLES
- * 4. SHAREPOINT
- * 5. UKEF_ID
+ * 1. ALLOWED_DOCUMENT_FILE_TYPE
+ * 2. AUTH
+ * 3. CASE_LIBRARY
+ * 4. DOCUMENT_X0020_STATUS
+ * 5. DTFS_MAX_FILE_SIZE_BYTES
+ * 6. ENUMS
+ * 7. EXAMPLES
+ * 8. SHAREPOINT
+ * 9. UKEF_ID
  */
 
+export * from './allowed-document-file-type.constant';
 export * from './auth.constant';
 export * from './case-library.constant';
 export * from './document-x0020-status.constant';
